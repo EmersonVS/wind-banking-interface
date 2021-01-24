4d31e2513161ef75d7a107c2e65b499bc3d089b1
EmersonVS <eme_valentim@hotmail.com>
2021-01-24 12:41:41 -0300

    build: bootstrap and jquery added

---
+++ src/app/feature/welcome-module/welcome-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-module',
+  templateUrl: './welcome-module.component.html',
+  styleUrls: ['./welcome-module.component.scss']
+})
+export class WelcomeModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
