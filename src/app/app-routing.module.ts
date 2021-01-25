6aee66a950303f96e6fd409d4cfcf2accb34613f
EmersonVS <eme_valentim@hotmail.com>
2021-01-24 22:36:59 -0300

    feat: tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './feature/account/account.component';
+import { DemoComponent } from './feature/account/demo/demo.component';
+import { WelcomeComponent } from './feature/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'account',
+    children: [
+      {
+        path: '',
+        component: AccountComponent
+      },
+      {
+        path: 'demo',
+        component: DemoComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'corrected' })],
--- src/app/feature/account/account.component.ts
+++ src/app/feature/account/account.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    // $('#login-card').css('height', `${screen.height}px`)
   }
 
 }
